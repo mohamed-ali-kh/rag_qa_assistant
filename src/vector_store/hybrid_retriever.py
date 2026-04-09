b6b61358e57895ad385ebe1faa7ecea059e43197
mohamed-ali-kh <mohammad153ali2006@gmail.com>
2026-04-10 00:17:27 +0200

    updated hybrid_retriever.py to use reranker

--- src/vector_store/hybrid_retriever.py
+++ src/vector_store/hybrid_retriever.py
@@ -2,6 +2,9 @@ from langchain_community.retrievers import BM25Retriever
 from langchain_classic.retrievers import EnsembleRetriever
 from langchain_community.vectorstores import FAISS
 from src.embeddings.models import create_embedding_model
+from langchain_community.document_compressors import FlashrankRerank
+from langchain_community import ContextualCompressionRetriever
+
 import os
 
 def get_hybrid_retriever(index_dir):
@@ -20,7 +23,7 @@ def get_hybrid_retriever(index_dir):
     faiss_retriever = vector_store.as_retriever(
         search_type="mmr",
         search_kwargs={
-                        "k": 5,
+                        "k": 6,  # fetch more candidates for reranker to work with
                         "fetch_k": 20,
                         "lambda_mult": 0.7
                         }
@@ -30,11 +33,18 @@ def get_hybrid_retriever(index_dir):
     # 2- build BM25 retriever for keyword search
     docs  = list(vector_store.__dict.values())   #vector_store.docstore._dict is FAISS's internal document store — a plain Python dict where the keys are chunk IDs and the values are the actual Document objects (the text chunks + metadata). Calling .values() gives you all the documents, and wrapping it in list() converts it to a list.
     bm25_retriever = BM25Retriever.from_documents(docs)
-    bm25_retriever.k = 5
+    bm25_retriever.k = 6
 
     hybrid_retriever = EnsembleRetriever(
         retrievers=[faiss_retriever, bm25_retriever],
         weights=[0.6, 0.4]  # Adjust weights as needed
     )
-    return hybrid_retriever
-
+    
+    # 3- add a compression layer to re-rank and filter results
+    # 4. Rerank the combined results down to the best 6
+    compressor = FlashrankRerank(top_n=4)  # Adjust top_n as needed
+    reranker = ContextualCompressionRetriever(
+        base_compressor=compressor,
+        base_retriever=hybrid_retriever
+    )
+    return reranker
