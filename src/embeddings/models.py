7782c81e6723ad3b8552b8d5a5d75545f215d167
mohamed-ali-kh <mohammad153ali2006@gmail.com>
2026-04-09 23:07:21 +0200

    updated create_memory_store to use redis

--- src/embeddings/models.py
+++ src/embeddings/models.py
@@ -1,5 +1,8 @@
+from functools import lru_cache
 from langchain_huggingface import HuggingFaceEmbeddings
 
+
+@lru_cache(maxsize=1)   #@lru_cache(maxsize=1) means the model is loaded once and reused everywhere
 def create_embedding_model():
     embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     return embedding_model
