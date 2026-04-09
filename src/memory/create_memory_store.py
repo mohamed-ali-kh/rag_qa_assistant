c4b6f1f259db10f05f15e7290fc8e140d2f71ee4
mohamed-ali-kh <mohammad153ali2006@gmail.com>
2026-04-09 23:02:45 +0200

    updated create_memory_store to use redis

--- src/memory/create_memory_store.py
+++ src/memory/create_memory_store.py
@@ -1,7 +1,24 @@
-from langchain_community.chat_message_histories import ChatMessageHistory
+#from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_redis import RedisChatMessageHistory
+import  os 
+from dotenv import load_dotenv
 
-store = {}
+load_dotenv()
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+
+"""store = {}  # wiped on every restart
 def get_session_id(session_id):
     if session_id not in store:
         store[session_id] = ChatMessageHistory()
-    return store[session_id]
+    return store[session_id]"""
+
+def get_session_id(session_id):
+    return RedisChatMessageHistory(
+        session_id=session_id,
+        redis_url=REDIS_URL,
+        ttl=60 * 60 * 24  # 1 day
+    )
+
+def clear_session_history(session_id):
+    history = get_session_id(session_id)
+    history.clear()
